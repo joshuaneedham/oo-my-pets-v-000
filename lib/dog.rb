6e9c589658dab85eae474d4246133dc4e5dd11fe
Joshua Needham <joshuabneedham@gmail.com>
2017-11-13 21:00:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,6 +8,6 @@ class Dog
   end
 
   def self.mood
-    @mood   
+    @mood
   end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
 class Fish
   # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name)
+    @name = name
+    @mood = mood
+  end
+
+  def self.mood
+    @mood   
+  end
 end
