f789ad48842366ba31de93a41d44cc61e181cab2
Joshua Needham <joshuabneedham@gmail.com>
2017-11-13 22:37:05 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,9 +3,16 @@ class Owner
   attr_accessor :pets
 
   @@pets
-
+  @@owners
   def initialize(name)
     @name = name
     @pets = {}
+    @@owners = 0
+  end
+
+  def self.count
+    @@owners
+  end
+  def species
   end
 end
