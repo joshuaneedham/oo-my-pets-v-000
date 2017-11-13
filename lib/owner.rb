5698a7f159b5e1915f87d6e90e5123b59097574a
Joshua Needham <joshuabneedham@gmail.com>
2017-11-13 22:40:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,10 +9,46 @@ class Owner
     @pets = {}
     @@owners = 0
   end
+  
+  def species
+  end
 
+  def say_species
+  end
+
+  def name
+    @name
+  end
+
+  def pets
+  end
+
+  def buy_fish
+  end
+
+  def buy_cat
+  end
+
+  def buy_dog
+  end
+
+  def walk_dogs
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+    puts @pets
+  end
+  
   def self.count
     @@owners
-  end
-  def species
   end
 end
