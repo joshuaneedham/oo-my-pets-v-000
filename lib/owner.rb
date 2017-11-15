7376513dcb2055654a82d6c39060eeb76c29d096
Joshua Needham <joshuabneedham@gmail.com>
2017-11-15 20:09:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,6 +63,13 @@ class Owner
     end
   end
 
+  def say_species
+    "I am a #{species}."
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+
 
 
 
